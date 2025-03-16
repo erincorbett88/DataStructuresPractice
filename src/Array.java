7c52d1eaf8fc693efbfe53611b15a84b50905318
Erin Corbett <ecorbett.ctr@kr.af.mil>
2025-03-16 17:04:25 -0600

    added a removeAt function to array

--- src/Array.java
+++ src/Array.java
@@ -4,22 +4,41 @@ public class Array {
 
     private int count;
 
-    //initializer
+    //initialize
     public Array(int length) {
         items = new int[length];
     }
 
     public void insert(int item) {
-        //if array is full, resize it
-        //add new item at the end of current array
+        if (items.length == count) {
+            int[] newItems = new int[count*2];
+            System.out.println("resized array to " + count*2);
+            for (int i = 0; i < count; i++) {
+                newItems[i] = items[i];
+            }
+            items = newItems;
+        }
         items[count] = item;
         count++;
-        //can also say items[count++]=item;
+    }
 
+    public void removeAt(int index){
+        //validate index is in correct range (ie can't be negative one, or bigger than size of array)
+        if ( index < 0 || index >= count )
+            throw new IllegalArgumentException();
+        // i - index just copy
+        // at index, items[i] = items i++
+        // if index==count, i = 0
+        for (int i=index; i < count; i++) {
+            items[i] = items[i + 1];
+        }
+        count--;
+        //this count-- will change what is printed in the print function
+        //actual array items holds on to this value unless changed to zerp
     }
 
     public void print() {
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < items.length; i++) {
             System.out.println(items[i]);
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         newArray.insert(20);
         newArray.insert(30);
         newArray.insert(40);
+        newArray.removeAt(1);
         newArray.print();
     }
 }
